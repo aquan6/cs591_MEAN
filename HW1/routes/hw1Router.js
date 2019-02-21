da194dc7c02e439733807c36de56a652327b67f1
Andy - Mac <aquan@bu.edu>
2019-02-21 16:39:36 -0500

    updating hw1 router

--- HW1/routes/hw1Router.js
+++ HW1/routes/hw1Router.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+//res.render or res.send breaks the chain of looking for pattern matching.
+
 /* GET constant JSON object */
 router.get('/hw1', function(req, res, next) {
-    res.render('index', { string: 'Hey now' });
+    res.render('hw1', { title: 'Hey now' });
 });
 
 /* POST the URL string */
-router.post('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.post('/hw1', function(req, res, next) {
+    res.render('hw1', 'respond with a resource');
 });
-module.exports = router;
+
+module.exports = router; //gives access to app.js
