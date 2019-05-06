a0f8105fcdc27cd3571c6d36aede0140ad93f7e5
Andy - Mac <aquan@bu.edu>
2019-05-06 18:02:55 -0400

    adding login page

--- hw5_nodeExpress/app.js
+++ hw5_nodeExpress/app.js
@@ -3,11 +3,6 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-const hw3Router = require('./routes/hw3Router');
-
 let app = express();
 
 //setup oauth
@@ -66,27 +61,26 @@ passport.deserializeUser(function(obj, cb) {
 //routes for oauth
 app.get('/',
     function(req, res){
-      res.render('../views/query', { user: req.user });
+      res.render('views/query', { user: req.user });
     });
 
-
-app.get('/logout', (req, res) => {
-  req.logout();
-  req.session = null;
-  res.render('../views/query');
-});
 
 app.get('/auth/google', passport.authenticate('google', {
   scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
 }));
 
 app.get('/auth/google/redirect', passport.authenticate('google', {
-      failureRedirect: '/login'}),
+      failureRedirect: 'views/login'}),
     (req, res) => {
       req.session.token = req.user.token;
-      res.redirect('/homepage');
+      res.redirect('views/query');
     }
 );
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const hw3Router = require('./routes/hw3Router');
+const loginRouter = require('./routes/login');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -101,6 +95,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hw3', hw3Router);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ hw5_nodeExpress/routes/login.js
@@ -0,0 +1,23 @@
+const express = require('express'),
+    app = express();
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/cs591', {useNewUrlParser: true}, function(err, db) {
+    if (err) {
+        console.log('Unable to connect to the server. Please start the server. Error:', err);
+    }
+});
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully!');
+});
+
+app.set('view engine', 'ejs');
+
+
+module.exports = app;
